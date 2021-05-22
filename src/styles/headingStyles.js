fc8a4411ef2dbb83eed2349935ddcf4e403becea
MozartOnWeb <mozartontheweb@gmail.com>
2021-05-22 02:03:32 +0100

    first commit

--- src/styles/headingStyles.js
+++ src/styles/headingStyles.js
@@ -1,6 +1,9 @@
 // Import Styled Cpmponents
 import styled from "styled-components";
 
+// Import BreakPoint
+import {down} from "styled-breakpoints"
+
 export const Header = styled.div`
   width: 80vw;
   text-align: center;
@@ -12,8 +15,14 @@ export const Title = styled.h1`
   font-size: 22px;
   text-transform: uppercase;
   margin-bottom: 10px;
+  ${down('sm')} {
+  font-size: 18px;
+  }
 `;
 
 export const Subtitle = styled.p`
   font-weight: 300;
+  ${down('sm')} {
+  font-size: 14px;
+  }
 `;
